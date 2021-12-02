d9dde6f48c27b519705e59613562f131ea11a48a
MarekUrbas <83548156+MarekUrbas@users.noreply.github.com>
2021-12-02 23:14:32 +0100

    Unity - tirer

---
+++ Strzelanie.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strzelanie : MonoBehaviour
+{
+    [SerializeField] private Transform miejsceStrzalu;
+
+    [SerializeField] private GameObject nasz_particle;
+
+    private RaycastHit cel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (Physics.Raycast(miejsceStrzalu.position, miejsceStrzalu.forward, out cel))
+            {
+                var go = Instantiate(nasz_particle, cel.point, Quaternion.identity);
+                go.transform.rotation = Quaternion.LookRotation(cel.normal.normalized);
+            }
+        }
+    }
+
+    
+}
